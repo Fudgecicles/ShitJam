5af3950bc6652c38bbd379515e5f43d85c3137be
Steven Li <listeven@usc.edu>
2016-09-17 14:45:36 -0700

    timer

---
+++ Assets/Scripts/TheActualGame.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class TheActualGame : MonoBehaviour {
+	public float timer;
+	public Text guitext;
+	bool gamestarted;
+	// Use this for initialization
+	void Start () {
+		gamestarted = false;
+		guitext.text = "";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.Space)) {
+			gamestarted = true;
+		}
+		if (gamestarted) {
+			timer -= Time.deltaTime;
+			guitext.text = "Time Left: " + Mathf.RoundToInt (timer).ToString ();
+		}
+	}
+		
+}
