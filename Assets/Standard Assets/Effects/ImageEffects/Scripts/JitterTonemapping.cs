80843254c0cb5f163741783b4720e54cd49cd105
Steven Li <listeven@usc.edu>
2016-09-16 21:14:34 -0700

    I MADE IT PRETTY WITH FREE SHIT

---
+++ Assets/Standard Assets/Effects/ImageEffects/Scripts/JitterTonemapping.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets.CinematicEffects;
+
+public class JitterTonemapping : MonoBehaviour {
+
+    TonemappingColorGrading tonemapping;
+    float initialWhiteIn;
+    public float jitterMax;
+
+	// Use this for initialization
+	void Start () {
+        tonemapping = GetComponent<TonemappingColorGrading>();
+        initialWhiteIn = tonemapping.tonemapping.neutralWhiteIn;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        TonemappingColorGrading.TonemappingSettings settings = tonemapping.tonemapping;
+        settings.neutralWhiteIn = initialWhiteIn + Random.Range(-jitterMax, jitterMax);
+        tonemapping.tonemapping = settings;
+        Debug.Log("running");
+	}
+}
