b8046c8c9e98af8d7f6672db71c30d9d5e484dd3
Steven Li <listeven@usc.edu>
2016-09-16 20:57:20 -0700

    "asfd"

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+
+public class Menu : MonoBehaviour {
+	string fileData;
+	string[] lines;
+	string[] lineData;
+
+	Drink[] drinkList;
+	// Use this for initialization
+
+	void Start () {
+		fileData  = System.IO.File.ReadAllText("IngredientList.csv");
+		lines = fileData.Split("/n");
+		lineData = (lines[0].Trim()).Split(","[0]);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
