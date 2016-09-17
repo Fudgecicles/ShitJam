beeeba2e6cb97d09a5f4fd7651f28a746794c093
Steven Li <listeven@usc.edu>
2016-09-17 15:42:44 -0700

    transition

---
+++ Assets/LoadGameScene.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadGameScene : MonoBehaviour {
+
+    public void LoadGame()
+    {
+        Debug.Log("called");
+        Application.LoadLevel(2);
+    }
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
